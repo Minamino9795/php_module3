c80ae99b0ec83464626c907b86fef5eb402ada97
Minamino9795 <minamino9795@gmail.com>
2023-08-14 09:32:02 +0700

    1

---
+++ B1_class_object/demo.php
@@ -0,0 +1,31 @@
+<?php
+
+//khai báo lớp đối tượng
+
+// class SinhVien {
+//     private $ten; // Thuộc tính tên
+
+//     // Phương thức để đặt tên
+//     public function setName($ten) {
+//         $this->ten = $ten;
+//     }
+
+//     // Phương thức để lấy tên
+//     public function getName() {
+//         return $this->ten;
+//     }
+// }
+
+// // Tạo một đối tượng từ lớp SinhVien
+// $sv = new SinhVien();
+
+// // Thiết lập tên cho đối tượng
+// $sv->setName("Nguyen Van A");
+
+// // Lấy và in ra tên từ đối tượng
+// $tenSinhVien = $sv->getName();
+// echo "Tên sinh viên: " . $tenSinhVien;
+
+
+
+
